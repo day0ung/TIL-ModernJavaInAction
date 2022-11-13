dbb916e147fa4db0bc578d1e7a37c95380d1c843
day0ung <jdy9436@naver.com>
2022-11-13 17:22:11 +0900

    [java_code] 2-1퀴즈

--- java_code/modern_java/src/chapter02/SourceCode022.java
+++ java_code/modern_java/src/chapter02/SourceCode022.java
@@ -15,6 +15,23 @@ public class SourceCode022 {
         //2.2.1
         List<Apple> redAndHeabyApple = filterApples(inventory, new AppleRedAndHeavyPredicate());
         System.out.println(redAndHeabyApple); //[Apple{color=RED, weight=180}]
+
+        //퀴즈 (2-1) 유연한 prettyPrintApple 메서드 구하기
+        /*
+        사과 리스트를 인수로 받아 다양한 방법으로 문자열을 생성(커스터마이즈된 다양한 toString메서드와 같이)할수 있도록 파라미터화된
+        prettryPrintApple 메서드를 구현
+         */
+        prettryPrintApple(inventory, new AppleSimpleFormatter());
+        //An apple of 80g
+        //An apple of 155g
+        //An apple of 180g
+        //An apple of 120g
+        prettryPrintApple(inventory, new AppleFancyFormatter());
+        //A light GREEN apple
+        //A heavt GREEN apple
+        //A heavt RED apple
+        //A light RED apple
+        //
     }
 
 
@@ -55,4 +72,31 @@ public class SourceCode022 {
             return apple.getColor() == Color.RED && apple.getWeight() > 150;
         }
     }
+
+    //퀴즈 2-1
+    public interface AppleFormatter{ String accept(Apple apple); };
+
+    static class AppleFancyFormatter implements AppleFormatter{
+        @Override
+        public String accept(Apple apple) {
+            String characteristic = apple.getWeight() > 150 ?  "heavt" : "light";
+
+            return "A "+ characteristic + " " + apple.getColor() + " apple";
+        }
+    }
+
+    static class AppleSimpleFormatter implements AppleFormatter{
+        @Override
+        public String accept(Apple apple) {
+            return "An apple of " + apple.getWeight() + "g";
+        }
+    }
+    //method
+    public static void prettryPrintApple(List<Apple> inventory, AppleFormatter formatter){
+        for (Apple apple : inventory) {
+            String output = formatter.accept(apple);
+            System.out.println(output);
+        }
+    }
+
 }

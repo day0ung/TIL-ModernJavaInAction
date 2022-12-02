5b852525bf08bf15db0df2c469e6f4ac9ea346a1
day0ung <jdy9436@naver.com>
2022-12-02 23:42:15 +0900

    [java_code] source033

--- java_code/modern_java/src/chapter03/SourceCode033.java
+++ java_code/modern_java/src/chapter03/SourceCode033.java
@@ -8,6 +8,7 @@ public class SourceCode033 {
     private static final String FILE = SourceCode033.class.getResource("./data.txt").getFile();
 
     //3.3 실행어라운드 패턴
+
     public static void main(String[] args) throws IOException {
 
         System.out.println(processFile());
@@ -19,5 +20,5 @@ public class SourceCode033 {
             return br.readLine(); //<-실제 필요한 작업을 하는 행
         }
     }
-
+    //file을 열고 닫으려면 connection이 open/close의 동작은 항상 필요하다.
 }

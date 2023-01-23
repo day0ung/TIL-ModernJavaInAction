caf4b83a1bcc069672c537cba003de6845abb18e
day0ung <jdy9436@naver.com>
2023-01-23 15:12:06 +0900

    [java_code] SourceCode056

--- java_code/modern_java/src/chapter05/SourceCode056.java
+++ java_code/modern_java/src/chapter05/SourceCode056.java
@@ -1,6 +1,66 @@
 package chapter05;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+
 public class SourceCode056 {
+    public static void main(String[] args) {
+        Trader raoul = new Trader("Raoul", "Cambridge");
+        Trader mario = new Trader("Mario", "Milan");
+        Trader alan = new Trader("Alan", "Cambridge");
+        Trader brian = new Trader("Brian", "Cambridge");
+        List<Transaction> transactions = Arrays.asList(
+                new Transaction(brian, 2011, 300),
+                new Transaction(raoul, 2012, 1000),
+                new Transaction(raoul, 2011, 400),
+                new Transaction(mario, 2012, 710),
+                new Transaction(mario, 2012, 700),
+                new Transaction(alan, 2012, 950)
+        );
+
+        //1. 2011년에 일어난 모든 트랜잭션을 찾아 오름차순으로 정리
+        List<Transaction> ex1 = transactions.stream()
+                .filter(transaction -> transaction.getYear() ==2011)
+                .sorted(Comparator.comparing(Transaction::getValue))
+                .collect(toList());
+        System.out.println(ex1); //[Transaction{trader=Trader{name='Brian', city='Cambridge'}, year=2011, value=300}, Transaction{trader=Trader{name='Raoul', city='Cambridge'}, year=2011, value=400}]
+
+
+        //2. 거래자가근무하는 모든 도시를 중복없이 나열
+        List<String> ex2 = transactions.stream()
+                .map(transaction -> transaction.getTrader().getCity())
+                .distinct()
+                .collect(toList());
+        System.out.println(ex2); //[Cambridge, Milan]
+
+        //3.케임브릿지에서 근무하는 모든 거래자를 찾아서 이름순으로 정렬
+        List<String> ex3 = transactions.stream()
+                .map(Transaction::getTrader)
+                .filter(trader -> trader.getCity().equals("Cambridge"))
+                .distinct()
+                .sorted(Comparator.comparing(Trader::getName))
+                .map(Trader::getName)
+                .collect(toList());
+        System.out.println(ex3); //[Alan, Brian, Raoul]
+
+        //4. 모든 거래자의 이름을 알파벳순으로 정렬해서 반환
+        String ex4 = transactions.stream()
+                .map(transaction -> transaction.getTrader().getName())
+                .distinct()
+                .sorted()
+                .reduce("", (n1,n2) -> n1+n2); //이름을 알파벳순으로 정렬
+        System.out.println(ex4);
+
+
+
+    }
+
+
+
 
     static class Transaction{
         private Trader trader;
